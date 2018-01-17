0a7e873b0ab3cd91e34b13027d6a750238c9f07c
don9 <864426487@qq.com>
2018-01-17 14:04:06 +0800

    使用FutureTask重构客户端channel连接池,避免并发环境下channel重复创建

--- src/main/java/cn/booklish/sharp/remoting/netty4/core/ClientChannelPool.kt
+++ src/main/java/cn/booklish/sharp/remoting/netty4/core/ClientChannelPool.kt
@@ -1,38 +1,40 @@
 package cn.booklish.sharp.remoting.netty4.core
 
 import io.netty.channel.Channel
+import java.util.concurrent.Callable
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.FutureTask
 
 /**
  * 客户端连接池
  */
 object ClientChannelPool {
 
-    private val channelPoolMap = ConcurrentHashMap<String, Channel>()
+    private val channelPoolMap = ConcurrentHashMap<String, FutureTask<Channel>>()
 
     fun getChannel(serverAddress: String,serviceName: String): Channel {
 
         val key = serverAddress + "/" + serviceName
 
-        val channel = channelPoolMap[key]
+        var channelTask = channelPoolMap[key]
 
-        return if(channel==null || !channel.isOpen){
-
-            //如果channel为空或者不可用,那么创建新channel
-            val newChannel = Client.newChannel(serverAddress)
-            if(channelPoolMap.putIfAbsent(key,newChannel) != null){
-
-                //新channel添加失败,说明已经有其他线程优先添加了该服务对应的channel,
-                //那么关闭新建的channel,返回map中已存在的
-                newChannel.close()
-                channelPoolMap[key]!!
-            }else{
-
-                //新channel添加成功,直接返回新channel
-                newChannel
-            }
+        return if(channelTask != null){
+            //future非空,直接get获取连接
+            channelTask.get()
         }else{
-            channel
+            //否则创建future
+            val newTask = FutureTask<Channel>(Callable {
+                Client.newChannel(serverAddress)
+            })
+            //添加future到map
+            channelTask = channelPoolMap.putIfAbsent(key,newTask)
+            if(channelTask==null){
+                //添加成功,更新channelTask引用,并run建立channel连接
+                channelTask = newTask
+                channelTask.run()
+            }
+            //添加不成功,说明其他线程已经添加完毕,那么直接获取
+            channelTask.get()
         }
 
     }
