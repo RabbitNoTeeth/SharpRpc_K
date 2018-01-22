365911894c4fbb70e9ef5ef2751df7f406a681cd
don9 <864426487@qq.com>
2018-01-22 11:13:12 +0800

    更新注释说明

--- src/main/java/cn/booklish/sharp/remoting/netty4/core/Client.kt
+++ src/main/java/cn/booklish/sharp/remoting/netty4/core/Client.kt
@@ -29,6 +29,7 @@ object Client {
     init {
         bootstrap.group(eventLoopGroup)
                 .channel(NioSocketChannel::class.java)
+                //设置客户端连接的超时时间
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS,10000)
                 .option(ChannelOption.SO_KEEPALIVE, true)
                 .option(ChannelOption.TCP_NODELAY, true)
@@ -49,18 +50,22 @@ object Client {
 
             try{
                 val channelFuture = this.bootstrap.clone().connect(NettyUtil.resolveAddressString(registerValue.address)).sync()
+                        //添加监听器,判断连接是否成功,当连接超时时,进入if代码块,抛出异常
                         .addListener { future ->
-                    if (future.isDone && !future.isSuccess){
-                        throw IllegalStateException("连接到服务 $serviceName 的提供者 ${registerValue.address} 失败,尝试连接其他服务提供者")
-                    }
-                }
+                                        if (future.isDone && !future.isSuccess){
+                                            throw IllegalStateException("连接到服务 $serviceName 的提供者 ${registerValue.address} 失败,尝试连接其他服务提供者")
+                                        }
+                                    }
                 return channelFuture.channel()
             }catch (e:Exception){
+                //捕获连接异常,打印日志
                 logger.warn("连接到服务 $serviceName 的提供者 ${registerValue.address} 失败,尝试连接其他服务提供者")
+                //从提供者列表中删除当前服务提供者
                 providers.remove(registerValue)
                 if(providers.isEmpty()){
                     throw IllegalStateException("服务 $serviceName 无可用连接")
                 }
+                //重新尝试其他鼓舞提供者
                 x = random.nextInt(providers.size)
                 registerValue = providers[x]
             }
